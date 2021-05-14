c7e0492d62e8c27527d8011b3f198c071b8dd680
Samuel Bouchet <contact@samuel-bouchet.fr>
2021-05-14 09:34:05 +0200

    Add paintings

--- tools/extractData.js
+++ tools/extractData.js
@@ -11,6 +11,7 @@ const all = getDirectories('src/img', function (res) {
     const detail = res.split('/')
     const gallery = detail[2]
     console.log(JSON.stringify(detail))
+    if (detail.length < 4) return null
     const description = detail[3].match(/[0-9]*\- ?(.*) \(/)
     const date = detail[3].match(/\((.*) \- /)
     const format = detail[3].match(/ \- ([0-9x]*)\)/)
@@ -19,12 +20,13 @@ const all = getDirectories('src/img', function (res) {
         "gallerie": gallery,
         "description": description.length > 1 ? description[1] : description[0],
         "format": format ? format[1] : "",
-        "date": date ? date[1] : "Inconnue"
+        "date": date ? date[1] : "Inconnue",
+        "en_premiere_page" : false
     }
 
 })
     .filter(obj => obj != null);
 let galleries = all.map(obj => obj.gallerie)
 galleries = galleries.filter((e, i) => galleries.indexOf(e) === i).map(g => ({name: g}));
-fs.writeFileSync("src/_data/paintings.json", JSON.stringify(all, null, 2), {encoding: 'utf8'})
-fs.writeFileSync("src/_data/galleries.json", JSON.stringify(galleries, null, 2), {encoding: 'utf8'})
+fs.writeFileSync("src/_data/paintings2.json", JSON.stringify(all, null, 2), {encoding: 'utf8'})
+//fs.writeFileSync("src/_data/galleries.json", JSON.stringify(galleries, null, 2), {encoding: 'utf8'})
